34e72a34f29ce09d8cb134c8e0fd7a100dd31072
deep <dipvadukiya06@gmail.com>
2026-05-12 14:24:36 +0200

    FTUC06: airport live dahboard: added new page, route and respective components

---
+++ src/pages/AirportDashboard/index.jsx
@@ -0,0 +1,17 @@
+//
+
+import { useEffect } from "react";
+
+// ----------------------------------------
+
+export default function AirportDashboard() {
+  useEffect(() => {
+    //
+  }, []);
+
+  return (
+    <div>
+      <div>AirportDashboard</div>
+    </div>
+  );
+}
