b8b1ad8ec15f3db6e2222a39ad377d875c7275e1
deep <dipvadukiya06@gmail.com>
2026-05-14 12:34:37 +0200

    FTUC03: airport route finder: added page component, added airport dataset

---
+++ src/pages/SearchRoutes/index.jsx
@@ -0,0 +1,11 @@
+//
+
+import React from "react";
+
+// ----------------------------------------
+
+export default function index() {
+  console.log("Search Routes page");
+
+  return <div>Airport route finder</div>;
+}
