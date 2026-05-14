361f9193912793a3143f9bec3cbd39c3c6496ea6
deep <dipvadukiya06@gmail.com>
2026-05-14 17:01:25 +0200

    FTUC03: airport route finder: added flight routes container and made it worked

--- src/pages/SearchRoutes/components/FlightConnection.jsx
+++ src/pages/SearchRoutes/components/FlightConnection.jsx
@@ -1,12 +1,16 @@
 //
 
-import { getFlightDuration, getTimeFormat } from "../../../utils/timeFormatter";
+import {
+  getFlightDuration,
+  getTimeFormat,
+  isNextDayArrival,
+} from "../../../utils/timeFormatter";
 
 // ----------------------------------------
 
 export default function FlightConnection({ flightRoutes }) {
   return (
-    <div>
+    <div style={{ maxWidth: 600 }}>
       {flightRoutes?.routes?.map((routes) => {
         return (
           <div
@@ -16,6 +20,7 @@ export default function FlightConnection({ flightRoutes }) {
               border: "1px solid black",
               borderRadius: 8,
             }}
+            key={`${routes?.source}-${routes?.destination}`}
           >
             {routes?.layovers === 0 ? <DirectFlights routes={routes} /> : null}
             {routes?.layovers === 1 ? <SingleLayover routes={routes} /> : null}
@@ -27,24 +32,25 @@ export default function FlightConnection({ flightRoutes }) {
 }
 
 const DirectFlights = ({ routes }) => {
-  console.log(routes);
-
   const segment = routes?.segments[0];
 
-  const departure = new Date(segment?.departure?.scheduled);
-  const arrival = new Date(segment?.arrival?.scheduled);
-
-  const isNextDay = departure.getUTCDate() !== arrival.getUTCDate();
-
   return (
-    <div>
+    <div style={{ alignItems: "center" }}>
       <div>
         <small>Direct Flight</small>
       </div>
 
-      <div style={{ display: "flex", gap: "20px", marginTop: "10px" }}>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          marginTop: "10px",
+        }}
+      >
         <div>
-          <div>{getTimeFormat(segment.departure.scheduled)}</div>
+          <div style={{ color: "black", fontWeight: "bold" }}>
+            {getTimeFormat(segment.departure.scheduled)}
+          </div>
           <div>{segment.from}</div>
         </div>
 
@@ -53,6 +59,7 @@ const DirectFlights = ({ routes }) => {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            width: 260,
           }}
         >
           <small style={{ color: "gray" }}>
@@ -66,15 +73,16 @@ const DirectFlights = ({ routes }) => {
         </div>
 
         <div>
-          <div>
-            <div>
-              {getTimeFormat(segment.arrival.scheduled)}
-              {isNextDay && (
-                <small style={{ color: "orange", marginLeft: 6 }}>+1 Day</small>
-              )}
-            </div>
-            <div>{segment.to}</div>
+          <div style={{ color: "black", fontWeight: "bold" }}>
+            {getTimeFormat(segment.arrival.scheduled)}
+            {isNextDayArrival(
+              segment?.departure?.scheduled,
+              segment?.arrival?.scheduled,
+            ) && (
+              <small style={{ color: "orange", marginLeft: 6 }}>+1 Day</small>
+            )}
           </div>
+          <div>{segment.to}</div>
         </div>
       </div>
     </div>
@@ -83,9 +91,67 @@ const DirectFlights = ({ routes }) => {
 
 const SingleLayover = ({ routes }) => {
   return (
-    <div>
-      <div>direct flight</div>
-      single layover
+    <div
+      style={{ alignItems: "center" }}
+      key={`${routes?.source}-${routes?.destination}`}
+    >
+      <div>
+        <small>One Layover</small>
+      </div>
+
+      {routes?.segments?.map((seg, i) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              gap: "20px",
+              marginTop: "10px",
+              paddingBottom: "10px",
+            }}
+            key={`${seg?.flight?.icao}-${i}`}
+          >
+            <div>
+              <div style={{ color: "black", fontWeight: "bold" }}>
+                {getTimeFormat(seg.departure.scheduled)}
+              </div>
+              <div>{seg.from}</div>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: 260,
+              }}
+            >
+              <small style={{ color: "gray" }}>
+                {getFlightDuration(
+                  seg.departure.scheduled,
+                  seg.arrival.scheduled,
+                )}
+              </small>
+              <hr style={{ margin: 0, width: "100%" }} />
+              <small style={{ color: "gray" }}>{seg?.airline?.name}</small>
+            </div>
+
+            <div>
+              <div style={{ color: "black", fontWeight: "bold" }}>
+                {getTimeFormat(seg.arrival.scheduled)}
+                {isNextDayArrival(
+                  seg?.departure?.scheduled,
+                  seg?.arrival?.scheduled,
+                ) && (
+                  <small style={{ color: "orange", marginLeft: 6 }}>
+                    +1 Day
+                  </small>
+                )}
+              </div>
+              <div>{seg.to}</div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
